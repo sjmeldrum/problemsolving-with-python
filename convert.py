1e55566f8974621d586e2c31559ce29c119eb584
Stevo <sjmeldrum@gmail.com>
2019-06-09 15:29:44 -0500

    Covert an uppercase string to unicode and back.

---
+++ convert.py
@@ -0,0 +1,27 @@
+# get input "Enter upper-case message to hide: "
+secretString = ""
+ostring = ""
+
+original = input("Enter a message in uppercase: ")
+
+# convert message into unicode
+
+
+for char in original:
+    secretChar = str(ord(char))
+    secretString = secretString + secretChar
+
+# print secret message as unicode
+
+print("Secret Message: ", secretString)
+
+
+# convert message back into a string
+
+for char in range(0, len(secretString)-1, 2):
+    ochar = chr(int(secretString[char] + secretString[char + 1]))
+    print(type(ochar))
+    ostring = ostring + str(ochar)
+print("ostring type: ", type(ostring))
+print("Original Message: ", ostring)
+
